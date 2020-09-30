57952ebf4ebfc5c1e699864adff1224cb98cdaf6
jose <joanmo2@inf.upv.es>
2020-09-30 19:48:52 +0200

    removed three axis helper

--- webgl/threejs/Practica3.js
+++ webgl/threejs/Practica3.js
@@ -245,7 +245,7 @@ function setCameras(ar){
    //scene.add( new THREE.AxisHelper(100));
    //nexo.add(new THREE.AxisHelper(100))
    //nexo.add(esfera);
-   antebrazoRobot.add(new THREE.AxisHelper(100))
+   //antebrazoRobot.add(new THREE.AxisHelper(100))
 
  }
 
